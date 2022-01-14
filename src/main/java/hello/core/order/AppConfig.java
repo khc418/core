89ca074537934603714d1e41da25324dcf72dd93
khc418 <schoan418@naver.com>
2022-01-14 23:17:25 +0900

    JAVA Config v2

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -12,16 +12,17 @@ import hello.core.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
-    }
-
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
     public DiscountPolicy discountPolicy() {
 // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
