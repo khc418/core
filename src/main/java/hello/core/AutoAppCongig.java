3449f1de3bb1bde0a1dc36780d88d07f262a615b
khc418 <schoan418@naver.com>
2022-01-16 21:02:12 +0900

    AutoConfig

---
+++ src/main/java/hello/core/AutoAppCongig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) // Configuration이 붙은 건 추가안한다
+public class AutoAppCongig {
+
+}
