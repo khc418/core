b46e21b6e0ccbc21285d924b8a5ad00185a3a6e1
khc418 <schoan418@naver.com>
2022-01-19 04:20:09 +0900

    web provider

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,33 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggersProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        // HttpServletRequest로 고객요청정보 확인가능
+        String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger= myLoggersProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+//        Thread.sleep(1000);
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
